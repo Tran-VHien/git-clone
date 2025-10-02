527fabe5877fee63e12050261d65f7de0f805fa7
Tran-VHien <hientranvan80@gmail.com>
2025-10-02 23:38:32 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,57 @@
 #include <Arduino.h>
+#include <OneButton.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED1_PIN 4
+#define LED2_PIN 5
+#define BUTTON_PIN 35
+
+OneButton button(BUTTON_PIN, true); // true = dùng INPUT_PULLUP
+
+bool ledState = false;      // trạng thái LED
+bool blinking = false;      // chế độ nháy
+int currentLed = LED1_PIN;  // mặc định điều khiển LED1
+unsigned long lastBlink = 0;
+
+void singleClick() {
+  blinking = false;
+  ledState = !ledState;
+  digitalWrite(currentLed, ledState ? HIGH : LOW);
+}
+
+void doubleClick() {
+  blinking = false;
+  // đổi LED được điều khiển
+  currentLed = (currentLed == LED1_PIN) ? LED2_PIN : LED1_PIN;
+  Serial.print("Đang điều khiển LED trên chân: ");
+  Serial.println(currentLed);
+}
+
+void longPress() {
+  blinking = true;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  digitalWrite(LED1_PIN, LOW);
+  digitalWrite(LED2_PIN, LOW);
+
+  button.attachClick(singleClick);
+  button.attachDoubleClick(doubleClick);
+  button.attachLongPressStart(longPress);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  button.tick();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (blinking) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastBlink >= 200) {
+      lastBlink = currentMillis;
+      ledState = !ledState;
+      digitalWrite(currentLed, ledState ? HIGH : LOW);
+    }
+  }
 }
